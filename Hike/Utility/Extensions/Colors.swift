fc79954eb2994aa5c6a0eca00ca877781ee2c017
Rafael Batista <maranata_rafa@hotmail.com>
2024-03-26 21:42:36 +0000

    FEAT: created cardview component

---
+++ Hike/Utility/Extensions/Colors.swift
@@ -0,0 +1,16 @@
+//
+
+import SwiftUI
+
+extension Color {
+    static let GREEN_LIGHT = Color("ColorGreenLight")
+    static let GREEN_MEDIUM = Color("ColorGreenMedium")
+    static let GREEN_DARK = Color("ColorGreenDark")
+
+    static let GRAY_LIGHT = Color("ColorGrayLight")
+    static let GRAY_MEDIUM = Color("ColorGrayMedium")
+
+    static let SALMON_LIGHT = Color("ColorSalmonLight")
+
+    static let INDIGO_MEDIUM = Color("ColorIndigoMedium")
+}
