857a270fe6fdeadc39afe063b33a9ab04ce29344
Rafael Batista <maranata_rafa@hotmail.com>
2024-04-02 19:18:14 +0100

    FEAT: create motion animation

--- Hike/Components/CustomCircleView.swift
+++ Hike/Components/CustomCircleView.swift
@@ -4,21 +4,26 @@ import SwiftUI
 
 struct CustomCircleView: View {
     @State private var isAnimationGradient: Bool = false
-    
+
     var body: some View {
-        Circle()
-            .fill(LinearGradient(
-                colors: [
-                    .INDIGO_MEDIUM,
-                    .SALMON_LIGHT],
-                startPoint: isAnimationGradient ? .topLeading : .bottomLeading,
-                endPoint: isAnimationGradient ? .bottomTrailing : .topTrailing))
-            .onAppear{
-                withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)){
-                    isAnimationGradient.toggle()
+        ZStack {
+            Circle()
+                .fill(LinearGradient(
+                    colors: [
+                        .INDIGO_MEDIUM,
+                        .SALMON_LIGHT],
+                    startPoint: isAnimationGradient ? .topLeading : .bottomLeading,
+                    endPoint: isAnimationGradient ? .bottomTrailing : .topTrailing))
+                .onAppear{
+                    withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)){
+                        isAnimationGradient.toggle()
+                    }
                 }
-            }
-        .frame(width: 256, height: 256)    }
+
+            MotionAnimationView()
+        }
+        .frame(width: 256, height: 256)
+    }
 }
 
 #Preview (traits: .sizeThatFitsLayout) {
